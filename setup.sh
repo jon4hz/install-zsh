52f4143690ba9d982b8cfcedf90bf630dcdfd5e0
Jonah <jonah.zuercher@gmail.com>
2019-01-15 15:36:44 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -74,9 +74,9 @@ then
   done
 fi 
 
-git clone https://github.com/robbyrussell/oh-my-zsh.git
-chmod +x oh-my-zsh/tools/install.sh
-./oh-my-zsh/tools/install.sh
+git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+cp ~/.zshrc ~/.zshrc.orig
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 x=1
 while [ $x == 1 ]
